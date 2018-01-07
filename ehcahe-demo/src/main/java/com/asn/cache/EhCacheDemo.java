dd9bb7a36202e499c3652244a2e821fbabc6b9cc
xuwentang <asnjudy@163.com>
2018-01-07 12:22:11 +0800

    配置spring-sample的springframework依赖

--- ehcahe-demo/src/main/java/com/asn/cache/EhCacheDemo.java
+++ ehcahe-demo/src/main/java/com/asn/cache/EhCacheDemo.java
@@ -1,5 +1,7 @@
 package com.asn.cache;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import po.User;
@@ -9,17 +11,20 @@ import po.User;
  */
 public class EhCacheDemo {
 
+    private static Logger LOG = LoggerFactory.getLogger(EhCacheDemo.class);
+
 
     public static void testSpring() {
         // create and configure beans
-        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/applicationContext.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                        "classpath:spring/beans.xml");
         User user = context.getBean("user", User.class);
-        System.out.println(user);
+        LOG.info("user: " + user);
     }
 
     public static void main(String[] args) {
 
-
+        testSpring();
     }
 
 }
